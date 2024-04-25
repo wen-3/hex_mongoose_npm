67066e8d8903a016da57d5c6b043556983f815e9
wen-3 <sd63324@gmail.com>
2024-04-25 23:03:37 +0800

    find() 取得所有房型

--- server.js
+++ server.js
@@ -11,21 +11,34 @@ mongoose.connect('mongodb://127.0.0.1:27017/hotel')
         console.log(error);
     });
 
-Room.create(
-    {
-        name: "總統級別單人房",
-        price: 2000,
-        rating: 4.5
-    }
-).then(() => {
-    console.log("新增資料成功")
-}).catch(error => {
-    console.log(error)
-})
+// Room.create(
+//     {
+//         name: "總統級別單人房",
+//         price: 2000,
+//         rating: 4.5
+//     }
+// ).then(() => {
+//     console.log("新增資料成功")
+// }).catch(error => {
+//     console.log(error)
+// })
 
-const requestListener = (req, res) => {
-    console.log(req.url);
-    res.end();
+const requestListener = async (req, res) => {
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+       'Content-Type': 'application/json'
+     }
+    if(req.url="/rooms" && req.method=="GET"){
+        const rooms = await Room.find();
+        res.writeHead(200, headers);
+        res.write(JSON.stringify({
+            "status":"success",
+            rooms
+        }))
+        res.end();
+    }
 }
 
 const server = http.createServer(requestListener);
